19cc85f3c01c26d81e6466617dbabf81717763ca
davidski <davidski@deadheaven.com>
2014-12-03 11:58:02 -0800

    Correct metadata.rb typo

--- metadata.rb
+++ metadata.rb
@@ -4,9 +4,9 @@ maintainer_email 'davidski@deadheaven.com'
 license          'MIT'
 description      'Installs and configures vulnpryer'
 long_description 'Installs and configures vulnpryer'
-version          '0.1.1'
+version          '0.1.2'
 
 depends 'python', ">= 1.4.6"
 depends 'aws', '>= 2.4.0'
-depends 'chef-sugar', '>=2.0.0'
+depends 'chef-sugar', '>= 2.0.0'
 depends 'cron', '>= 1.4.0'
