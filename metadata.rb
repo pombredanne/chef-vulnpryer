385a27b9022c83ecc30a2e41bea9391e454f787f
davidski <davidski@deadheaven.com>
2014-12-05 19:23:02 -0800

    Formalize support for rhel and ubuntu

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,11 @@ description      'Installs and configures vulnpryer'
 long_description 'Installs and configures vulnpryer'
 version          '0.1.2'
 
+supports         'ubuntu'
+supports         'amazon'
+supports         'redhat'
+supports         'centos'
+
 depends 'python', ">= 1.4.6"
 depends 'aws', '>= 2.4.0'
 depends 'chef-sugar', '>= 2.0.0'
