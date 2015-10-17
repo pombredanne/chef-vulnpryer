8eb9247a39bf0c8cc6a6098d902b696e774c4468
davidski <davidski@deadheaven.com>
2015-10-17 12:03:05 -0700

    Pull in readme for long description

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       'David F. Severski'
 maintainer_email 'davidski@deadheaven.com'
 license          'MIT'
 description      'Installs and configures vulnpryer'
-long_description 'Installs and configures vulnpryer'
+long_description IO.read(File.join(File.dirname(__FILE__), ‘README.md‘))
 source_url       'https://github.com/SCH-CISM/chef-vulnpryer' if respond_to?(:source_url)
 issues_url       'https://github.com/SCH-CISM/chef-vulnpryer/issues' if respond_to?(:issues_url)
 version          '0.1.2'
